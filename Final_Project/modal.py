124dfe41279d1f0c56b37e6ff89485519b5a7e0e
ericknkunda <macnkunda@gmail.com>
2023-01-17 20:18:17 +0200

    random forest

--- Final_Project/modal.py
+++ Final_Project/modal.py
@@ -10,9 +10,12 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from numpy.linalg import svd
 import numpy as np
+import csv
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import f1_score
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 #connecting to the database
 
@@ -23,6 +26,9 @@ databaseConnection =pymysql.connect(
     database="final_project_new"
 )
 cursor =databaseConnection.cursor()
+cursor.execute("DESCRIBE fp_individual_visit_history")
+columns = [col[0] for col in cursor.fetchall()]
+
 visitHistoryQuerry ="select *from fp_individual_visit_history"
 
 try:
@@ -37,14 +43,13 @@ except:
     print("Error: Connection Failured")
 #creating a dataframe
 #by calling a pandas function
+with open('final_project.csv', 'w', newline='') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(columns)
+    csv_writer.writerows(historyDeatails)
 
-pandaFrame =pd.DataFrame(historyDeatails)
-pandaFrame.to_csv("user_item_interactions.csv", index=False)
-
-pandaFrame.columns = ['visit_id', 'user_id','phone_number', 'ages_range', 'item_id','item_visited', 'rating_given', 'visiting_times\
-    ', 'gender', 'residence','parent_category', 'sub_category', 'item_location','visiting_status']
-
-# ratings numeric
+ratings =pd.read_csv("final_project.csv", encoding = 'ISO-8859-1')
+# ratings =ratings.drop(['user_id'])
 rating_mapping = {
     "Very_Satisfied": 5,
     "Satisfied": 4,
@@ -77,90 +82,110 @@ residence_mapping = {
     "Urban": 2,
     "Rural": 1
 }
-pandaFrame["visiting_status"] = pandaFrame["visiting_status"].map(visitinf_status_mapping)
+ratings["item_visiting_status"] = ratings["item_visiting_status"].map(visitinf_status_mapping)
 # pandaFrame["ages_range"] = pandaFrame["ages_range"].map(ages_mapping)
-pandaFrame["rating_given"] = pandaFrame["rating_given"].map(rating_mapping)
-pandaFrame["gender"] = pandaFrame["gender"].map(gender_mapping)
-pandaFrame["residence"] = pandaFrame["residence"].map(residence_mapping)
+ratings["rating"] = ratings["rating"].map(rating_mapping)
+ratings["user_gender"] = ratings["user_gender"].map(gender_mapping)
+ratings["user_residence"] = ratings["user_residence"].map(residence_mapping)
+ratings['user_phone'] = pd.to_numeric(ratings['user_phone'])
 
-
-encoder_phone =OneHotEncoder()
 encoder_items = OneHotEncoder()
 encoder_location = OneHotEncoder()
 encoder_ages = OneHotEncoder()
 
-encoder_phone.fit(pandaFrame[['phone_number']])
-encoded_user_phone =encoder_phone.transform(pandaFrame[['phone_number']])
-df_encoded_user_phone = pd.DataFrame(encoded_user_phone.toarray())
-df_encoded_user_phone.columns = encoder_phone.get_feature_names_out(['phone_number'])
-# pandaFrame = pd.concat([pandaFrame, df_encoded_user_phone], axis=1)
-
-encoder_ages.fit(pandaFrame[['ages_range']])
-encoded_user_ages =encoder_ages.transform(pandaFrame[['ages_range']])
+encoder_ages.fit(ratings[['user_age']])
+encoded_user_ages =encoder_ages.transform(ratings[['user_age']])
 df_encoded_user_ages = pd.DataFrame(encoded_user_ages.toarray())
-df_encoded_user_ages.columns = encoder_ages.get_feature_names_out(['ages_range'])
-pandaFrame = pd.concat([pandaFrame, df_encoded_user_ages], axis=1)
+df_encoded_user_ages.columns = encoder_ages.get_feature_names_out(['user_age'])
+ratings = pd.concat([ratings, df_encoded_user_ages], axis=1)
 
 
 #scalling item visited
-encoder_items.fit(pandaFrame[['item_visited']])
-encoder_item_visited =encoder_items.transform(pandaFrame[['item_visited']])
+encoder_items.fit(ratings[['item_visited']])
+encoder_item_visited =encoder_items.transform(ratings[['item_visited']])
 
 df_item_visited =pd.DataFrame(encoder_item_visited.toarray())
 df_item_visited.columns = encoder_items.get_feature_names_out(['item_visited'])
 
-pandaFrame = pd.concat([pandaFrame, df_item_visited], axis=1)
+# ratings = pd.concat([ratings, df_item_visited], axis=1)
  
-encoder_location.fit(pandaFrame[['item_location']])
-encoder_item_location =encoder_location.transform(pandaFrame[['item_location']])
+encoder_location.fit(ratings[['item_location']])
+encoder_item_location =encoder_location.transform(ratings[['item_location']])
 df_item_location =pd.DataFrame(encoder_item_location.toarray())
 df_item_location.columns =encoder_location.get_feature_names_out(['item_location'])
 
-pandaFrame = pd.concat([pandaFrame, df_item_location], axis=1)
+ratings = pd.concat([ratings, df_item_location], axis=1)
+nonrated =ratings
 
-pandaFrame =pandaFrame.drop(['phone_number','ages_range','item_visited'],axis =1)
-newDataFrame =pandaFrame[['user_id','item_id', 'rating_given']]
-print(newDataFrame.head(5000))
-pandaFrameMatrix =pandaFrame.pivot(index=['visit_id','user_id'],columns='item_id', values='rating_given').fillna(0)
-# pandaFrameMatrix.dropna()
-# X = pandaFrameMatrix.drop(['rating_given'],axis=1)
-y = pandaFrame['rating_given'].fillna(0)
+ratings =ratings.pivot(index=['id'],columns='item_visited', values='rating')
+ratings =ratings.fillna(0)
+# print(ratings.head())
+# print(ratings.columns)
+
+def standardiserow(row):
+    new_row =(row-row.mean())/(row.max()-row.min())
+    return new_row
+
+ratings_std =ratings.apply(standardiserow)
+item_similarity =cosine_similarity(ratings_std.T)
+
+ratings_df =pd.DataFrame(item_similarity, index=ratings.columns, columns=ratings.columns)
+# ratings_df =ratings_df.fillna(0)
+# print(ratings_df.head())
+
+def get_similar_items(itemId, ratings_given):
+    similar_scores =ratings_df[itemId]*(ratings_given -2.5)
+    similar_scores =similar_scores.sort_values(ascending=False)
+    return similar_scores
+
+item_lover =[('Amahamba',4),('Abiru',4),('Museum',5)]
+similar_items =pd.DataFrame()
+for itemId, ratings_given in item_lover:
+    similar_items =similar_items.append(get_similar_items(itemId,ratings_given),ignore_index=True)
+    
+
+rowwise =similar_items.sum().sort_values(ascending=False)
+print("______________________________________")
+# print(rowwise) 
+
+
+y = nonrated['rating'].fillna(0)
 
 svd = TruncatedSVD(n_components=100, random_state=42)
-svd.fit(pandaFrameMatrix)
+svd.fit(ratings)
 
-X_transformed = svd.transform(pandaFrameMatrix)
+X_transformed = svd.transform(ratings)
+print(X_transformed.shape)
 X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, test_size=0.2, random_state=42)
 
 # X_train =X_train.fill(0)
 y_train =y_train.fillna(0)
 # X_test =X_test.fillna(0)
-y_test =y_test.fillna(0)
+y_test = y_test.fillna(0)
 reg = LinearRegression()
 reg.fit(X_train, y_train)
+
 print("Training score using regression: ", reg.score(X_train, y_train))
 
 y_train_pred = reg.predict(X_train)
 y_test_pred = reg.predict(X_test)
+# print('predictions on X_train',y_train_pred)
+# print('predictions on X_test',y_test_pred)
+
     
 # Evaluation Metrics
 print("MSE train: ", mean_squared_error(y_train, y_train_pred))
 print("MSE test: ", mean_squared_error(y_test, y_test_pred))
 
-# ...
 
-# Split the data into training and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X_train, y, test_size=0.2)
-
-# Initialize and fit the random forest regressor
-rf = RandomForestRegressor(n_estimators=100)
-rf.fit(X_train, y_train)
+rf = RandomForestRegressor(n_estimators=90)
+rf.fit(X_test, y_test)
 
 # Print the training score
 print("Training score using Random Forest: ", rf.score(X_train, y_train))
-
-# Print the test score
 print("Test score using Random Forest: ", rf.score(X_test, y_test))
+# print("Random forest predictions ",rf.predict(X_train) )
+# print("Random forest predictions ",rf.predict(X_test) )
 
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
@@ -169,70 +194,20 @@ from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 
 # Use the cross_val_score function to perform k-fold cross-validation
-scores = cross_val_score(reg, X_transformed, y, cv=5)
+scores = cross_val_score(rf, X_transformed, y, cv=5)
 
 # Print the mean and standard deviation of the scores
 print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
+rf.predict([[5.]*100])
 
-# print(pandaFrame.columns)
+# import pickle
 
-# #understanding the dataset
-
-# #head 5 rows
-# # print(X_train.columns)
-
-# #tail 5 rows
-# #print(pandaFrame.tail())
-
-# #summary of the data
-# # print(pandaFrame.info())
-
-# #description of numerical columns
-# # print(pandaFrame.describe())
-# print(pandaFrame["rating_given"].value_counts())
-# pandaFrame.dropna()
-# print(pandaFrameMatrix)
-
-# matrix = pandaFrameMatrix.values
-# u, s, vh = svd(matrix, full_matrices=False)
-# def cosine_similarity(v,u):
-#     return (v @ u)/ (np.linalg.norm(v) * np.linalg.norm(u))
- 
-# highest_similarity = -np.inf
-# highest_sim_col = -1
-# for col in range(1,vh.shape[1]):
-#     similarity = cosine_similarity(vh[:,0], vh[:,col])
-#     if similarity >= highest_similarity:
-#         highest_similarity = similarity
-#         highest_sim_col = col
- 
-# print("Column %d (item id %s) is most similar to column 0 (item id %s)" %
-#         (highest_sim_col, pandaFrameMatrix.columns[col], pandaFrameMatrix.columns[10]))
-# Specify the features and target
-# X = pandaFrame.drop(['rating_given'],axis=1)
-
-# Split the data into a training set and a test set
+# with open('model.pkl','wb') as f:
+#   pickle.dump(reg,f)
+# print("------------------------------------------")
+# print(get_similar_items(8,1))
 
 
-# # Instantiate the SVD model
-# print ("movie_features.shape = {0}".format(X_transformed.shape))
-# train_predictions = svd.predict(X_test)
-
-# calculate the MSE of the model on the training set
-# mse_train = mean_squared_error(X_test, train_predictions)
-# print("MSE train: ", mse_train)
-
-# Calculate the cosine similarity between latent feature vectors
-# item_similarities = cosine_similarity(X_transformed)
-
-# item_id = 5
-# top_n = 10
-# similar_items = np.argsort(-item_similarities[item_id])[1:top_n+1]
-# print(similar_items)
-
-# print(svd.singular_values_)
-# print(svd.explained_variance_ratio_)
-
-# interactionMatrix =X_train.pivot(index='user_id', columns ='item_id', values ='rating_given').fillna(0)
-# print(interactionMatrix)
+cursor.close()
+databaseConnection.close()
